b77a04d4bf6701070168e2729bd17e116652794f
Ralvesbraga <ralvesbraga40@gmail.com>
2024-09-25 22:20:37 -0500

    UnidadeDao semi-finalizada

--- sgcm/src/main/java/br/ufac/sgcm/dao/UnidadeDao.java
+++ sgcm/src/main/java/br/ufac/sgcm/dao/UnidadeDao.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UnidadeDao implements IDao<Unidade>{
@@ -20,7 +21,22 @@ public class UnidadeDao implements IDao<Unidade>{
 
     @Override
     public List<Unidade> get() {
-        return List.of();
+        String sql = "SELECT * FROM unidade";
+        List<Unidade> registros = new ArrayList<>();
+        try {
+            ps = conexao.prepareStatement(sql);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Unidade u = new Unidade();
+                u.setId(rs.getLong("id"));
+                u.setNome(rs.getString("nome"));
+                u.setEndereco(rs.getString("endereco"));   
+                registros.add(u);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return registros;
     }
     @Override
     public Unidade get(Long id){
@@ -43,22 +59,69 @@ public class UnidadeDao implements IDao<Unidade>{
 
     @Override
     public List<Unidade> get(String termoBusca) {
-        return List.of();
+        String sql = "SELECT * FROM unidade WHERE nome LIKE id=?";
+        List<Unidade> registros = new ArrayList<>();
+        try {
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, "%"+termoBusca+"%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Unidade u = new Unidade();
+                u.setId(rs.getLong("id"));
+                u.setNome(rs.getString("nome"));
+                u.setEndereco(rs.getString("endereco"));
+                registros.add(u);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return registros;
     }
 
     @Override
     public int insert(Unidade objeto) {
-        return 0;
+        String sql = "INSERT INTO unidade (nome, endereco) VALUES (?,?)";
+        int registrosAfetados = 0;
+        try {
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            registrosAfetados = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return registrosAfetados;
     }
 
     @Override
     public int update(Unidade objeto) {
-        return 0;
+        String sql = "UPDATE unidade SET nome=?, endereco=? WHERE id=?";
+        int registrosAfetados = 0;
+        try {
+            ps = conexao.prepareStatement(sql);
+            ps.setString(1, objeto.getNome());
+            ps.setString(2, objeto.getEndereco());
+            ps.setLong(3, objeto.getId());
+            registrosAfetados = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return registrosAfetados;
     }
 
     @Override
     public int delete(Unidade objeto) {
-        return 0;
+        String sql = "DELETE FROM unidade WHERE id=?";
+        int registrosAfetados = 0;
+        try {
+            ps = conexao.prepareStatement(sql);
+            ps.setLong(1,objeto.getId());
+            registrosAfetados = ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return registrosAfetados;
     }
 
 
